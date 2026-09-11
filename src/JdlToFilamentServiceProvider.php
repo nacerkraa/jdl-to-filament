a879828f2f8036e037711bb2c6af193154e45708
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2026-09-11 11:00:35 +0200

    Register DTO and service generators

--- src/JdlToFilamentServiceProvider.php
+++ src/JdlToFilamentServiceProvider.php
@@ -4,9 +4,11 @@ namespace Nacer\JdlToFilament;
 
 use Illuminate\Support\ServiceProvider;
 use Nacer\JdlToFilament\Console\Commands\GenerateCommand;
+use Nacer\JdlToFilament\Console\Commands\GenerateDtosCommand;
 use Nacer\JdlToFilament\Console\Commands\GenerateFilamentResourcesCommand;
 use Nacer\JdlToFilament\Console\Commands\GenerateMigrationsCommand;
 use Nacer\JdlToFilament\Console\Commands\GenerateModelsCommand;
+use Nacer\JdlToFilament\Console\Commands\GenerateServicesCommand;
 use Nacer\JdlToFilament\Console\Commands\InstallNodeCommand;
 use Nacer\JdlToFilament\Console\Commands\ScaffoldCommand;
 
@@ -19,13 +21,17 @@ class JdlToFilamentServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (! $this->app->runningInConsole()) return;
+        if (! $this->app->runningInConsole()) {
+            return;
+        }
 
         $this->commands([
             GenerateCommand::class,
             GenerateMigrationsCommand::class,
             GenerateModelsCommand::class,
             GenerateFilamentResourcesCommand::class,
+            GenerateDtosCommand::class,
+            GenerateServicesCommand::class,
             InstallNodeCommand::class,
             ScaffoldCommand::class,
         ]);
